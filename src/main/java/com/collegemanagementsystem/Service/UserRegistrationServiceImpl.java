e770df4094a2d57f3862afd84324ee0781ded232
gourav <gouravk842@gmail.com>
2021-04-30 10:01:19 +0530

    password encoder laayer added

--- src/main/java/com/collegemanagementsystem/Service/UserRegistrationServiceImpl.java
+++ src/main/java/com/collegemanagementsystem/Service/UserRegistrationServiceImpl.java
@@ -7,6 +7,7 @@ import com.collegemanagementsystem.Repository.UserRepository;
 import com.google.common.reflect.TypeToken;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.lang.reflect.Type;
 import java.util.List;
@@ -16,6 +17,8 @@ public class UserRegistrationServiceImpl implements UserRegistrationService{
 
     @Autowired
     private UserRepository userrepo;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
     @Autowired
     ModelMapper modelMapper;
     public List<UserRegistrationDto> allUser(){
@@ -31,7 +34,7 @@ public class UserRegistrationServiceImpl implements UserRegistrationService{
         UserRegistrationEntity newuser=new UserRegistrationEntity();
         newuser.setName(userdto.getName());
         newuser.setUsername(userdto.getUsername());
-        newuser.setPassword(userdto.getPassword());
+        newuser.setPassword(passwordEncoder.encode(userdto.getPassword()));
         newuser.setStatus(userdto.getStatus());
         newuser.setMobile(userdto.getMobile());
         return userrepo.save(newuser);
